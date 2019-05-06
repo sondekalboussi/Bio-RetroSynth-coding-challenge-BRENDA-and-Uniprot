2d16673e661c3adbcaa3cc4cda82b0c8f6b23b4c
sondes <sondekalboussi@users.noreply.github.com>
2019-05-06 11:44:08 +0200

    Update EC_inFasta.py

--- EC_inFasta.py
+++ EC_inFasta.py
@@ -8,16 +8,16 @@ import sys,os,re
 sys.path.insert(0, 'BRENDA-Parser')
 from urllib.request import urlopen
 from brenda.parser import BRENDAParser
-def EC_in_fasta():
+import collections
+
+def EC_infasta():
      '''
-     -Parse the string string designating the EC number
-     -Parse EC number from the comment and save non duplicated 
-     ones to a global variable
+     -Parse the string representing EC number
      -Parse UniProt accessions id using EC numbers
-     -get the fasta record of the Uniprot ID, 
+     -get the fasta of the Uniprot ID, 
      -append the corresponding EC number to the header and save the fasta
           file in the local machine
-        '''
+        '''    
      EC_final=[]
      print ('Brenda parser is starting')
      with BRENDAParser(brenda_flat) as parser:
@@ -28,44 +28,55 @@ def EC_in_fasta():
      
      ec_comment = [ec for ec in brenda if brenda[ec][0].comment is not None]
      for i in range(0,len(ec_comment)):
-          ec=brenda[ec_comment[0]][0].ec_number
-          EC=re.findall(r'\bEC\s[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\b',ec)#regular expression to parse EC from comment
-          for i in EC:
-               if i not in EC_final:#remove duplicated EC numbers
-                    EC_final.append(i)               
+          ec=brenda[ec_comment[i]][0].ec_number
+          #ec=brenda[ec_comment[i]][0].comment#ec number in comment
+          #EC=re.findall(r'\bEC\s[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\b',ec)#regular expression to parse EC from comment
+          for i in ec:
+            if i not in EC_final:#remove duplicated EC numbers
+               EC_final.append(i)               
      print(str(len(EC_final)),'EC number were parsed.')
      if not os.path.isdir(fast_output):
               os.makedirs(fast_output)     
-     print ('Extractin of uniprot identifiers and Fasta file modification')
-     unip_EC={}#key are uniprotid,value is EC
+     print ('Extraction of uniprot identifiers and fasta modification')   
+     Uniprot_EC={}#dic key is EC and value is uniprot id
      for e in EC_final:
-          EC_number=e.split("EC ")[1]#string of ec_numbers to be used to find the protein identifier
-          #if EC_number!='1.3.1.193':#this Ec is mentioned as new one in flat file comment but now is removed from the db
-          for prot_id in brenda[EC_number][0].proteins:
-               protein = brenda[EC_number][0].proteins[prot_id]
+          for prot_id in brenda[e][0].proteins:
+               protein = brenda[e][0].proteins[prot_id]
                uniprot_id=protein.identifiers#list of uniprot identifiers
-               uniprot_EC[e]=uniprot_id
                if len(uniprot_id)!=0:#for one EC number we can have more than one id depending on the organism
-                    try:  
-                         for id in uniprot_id:
-                                   url_with_id = "%s%s%s" %(uniprot_url, id, ".fasta") 
-                                   file_from_uniprot =urlopen(url_with_id)
-                                   fasta=file_from_uniprot.readlines()
-                                   f=open(fast_output+'/'+e+'_'+id+'.fasta','w')
-                                   for line in fasta:
-                                        line=line.decode('utf-8')#remove the Bytes literals,b', from lines 
-                                        if line.startswith('>'):
-                                           header=line.replace(line.strip(),line.strip()+' '+e.replace(' ','='))
-                                           f.write(header)
-                                        else:
-                                           fasta_seq=line.strip()
-                                           f.write(fasta_seq)
-                                   f.close()                             
-                    except Exception as error:
-                         print (str(error))
-     #print ('EC numbers are now added to Fasta headers')
+                    Uniprot_EC[e]=uniprot_id
+     Multi_EC= collections.defaultdict(list)#Multi EC number for one uniprot id
+     for key,value in Uniprot_EC.items():
+         for i in value:
+            Multi_EC[i].append(key)# reversed dic with key is uniprot id and value is list of EC multiple numbers 
+     for n,m in Multi_EC.items():
+          try:
+                url_with_id = "%s%s%s" %(uniprot_url,n, ".fasta") 
+                file_from_uniprot =urlopen(url_with_id)
+                fasta=file_from_uniprot.readlines()
+                f=open('fasta_output\\'+n+'.fasta','w+')
+                for line in fasta:
+                    line=line.strip().decode('utf-8')
+                    if line.startswith('>'):
+                        if len(m)>1:                                            
+                            print(n,' has multiple EC number') 
+                            header= line.replace(line,line+' '+' '.join([j.replace(' ','=') for j in m]))  
+                            f.write('%s\n' % (header))
+                        else: 
+                            header=line.replace(line,line+' '+''.join(m).replace(' ','='))
+                            f.write('%s\n' % (header))
+                    else:
+                           fasta_seq=line.strip()
+                           f.write('%s\n' %(fasta_seq))
+                f.close()                     
+                                 
+          except Exception as error:
+                 print (str(error))
+
+     return ('EC numbers are now added to Fasta headers')                  
 if __name__ == "__main__":
      uniprot_url = "http://www.uniprot.org/uniprot/"  # constant Uniprot Namespace
      brenda_flat='brenda_download.txt'
      fast_output='EC_unip_fasta'
-     print (EC_in_fasta())
+     print (EC_infasta())
+
